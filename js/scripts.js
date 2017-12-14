e9fa158155e92969a82da2cad7b2f57a5a56b106
Byron Chang and Jun Li <jun@>
2017-12-13 16:47:33 -0800

    refactor if else

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,32 @@
 var playerX = [];
 var playerO = [];
 
-var playerTurn = function(block){
+var playerTurn = function(block) {
   var turn = playerX.length + playerO.length;
   if (turn % 2 === 0) {
     playerX.push(block);
+    markPlayerX(block);
     checkWin(playerX);
-    return "url('img/imgx.png')";
   } else if (turn % 2 === 1) {
     playerO.push(block);
+    markPlayerO(block);
     checkWin(playerO);
-    return "url('img/imgo.png')";
   } else {
     console.log("broken");
   }
+}
+
+var victory = function() {
+
 }
 
 var checkWin = function(player) {
   if (player.indexOf("A1") > -1 && player.indexOf("A2") > -1 && player.indexOf("A3") > -1 ) {
     console.log("Win!");
   } else {}
+  // if ("A1" in player) {
+  //   console.log("Win!");
+  // } else {}
   if (player.indexOf("B1") > -1 && player.indexOf("B2") > -1 && player.indexOf("B3") > -1 ) {
     console.log("Win!");
   } else {}
@@ -43,59 +50,58 @@ var checkWin = function(player) {
   } else {}
 };
 
+var markPlayerX = function(location) {
+  $("#" + location).css("background-image", "url('img/imgx.png')");
+}
+var markPlayerO = function(location) {
+  $("#" + location).css("background-image", "url('img/imgo.png')");
+}
+
 $(document).ready(function() {
-  $("#a1").one("click", function() {
+
+  $("#A1").one("click", function() {
     console.log("A1");
     var block = "A1";
     var image = playerTurn(block);
-    $("#a1").css("background-image", image);
   });
-  $("#a2").one("click", function() {
+  $("#A2").one("click", function() {
     console.log("A2");
     var block = "A2";
     var image = playerTurn(block);
-    $("#a2").css("background-image", image);
   });
-  $("#a3").one("click", function() {
+  $("#A3").one("click", function() {
     console.log("A3");
     var block = "A3";
     var image = playerTurn(block);
-    $("#a3").css("background-image", image);
   });
-  $("#b1").one("click", function() {
+  $("#B1").one("click", function() {
     console.log("B1");
     var block = "B1";
     var image = playerTurn(block);
-    $("#b1").css("background-image", image);
   });
-  $("#b2").one("click", function() {
+  $("#B2").one("click", function() {
     console.log("B2");
     var block = "B2";
     var image = playerTurn(block);
-    $("#b2").css("background-image", image);
   });
-  $("#b3").one("click", function() {
+  $("#B3").one("click", function() {
     console.log("B3");
     var block = "B3";
     var image = playerTurn(block);
-    $("#b3").css("background-image", image);
   });
-  $("#c1").one("click", function() {
+  $("#C1").one("click", function() {
     console.log("C1");
     var block = "C1";
     var image = playerTurn(block);
-    $("#c1").css("background-image", image);
   });
-  $("#c2").one("click", function() {
+  $("#C2").one("click", function() {
     console.log("C2");
     var block = "C2";
     var image = playerTurn(block);
-    $("#c2").css("background-image", image);
   });
-  $("#c3").one("click", function() {
+  $("#C3").one("click", function() {
     console.log("C3");
     var block = "C3";
     var image = playerTurn(block);
-    $("#c3").css("background-image", image);
   });
 });
